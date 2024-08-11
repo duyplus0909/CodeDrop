fb5a296deb39af937189f2e868eecd3151964b1a
duyplus <duyplusdz@gmail.com>
2024-08-11 20:40:27 +0700

    Initial commit

---
+++ src/main/java/com/codedrop/CodeDropApplication.java
@@ -0,0 +1,13 @@
+package com.codedrop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeDropApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeDropApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codedrop/CodeDropApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codedrop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeDropApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
